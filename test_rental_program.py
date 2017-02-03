1d12bed6e785e66ed9f6e0803b24b86b3c6efebc
James Sibert <jsibert@basecampcodingacademy.org>
2017-02-03 11:00:26 -0600

    added new notes to each of the tests. Updated a few tests to be more accurate to the original functions.

--- test_rental_program.py
+++ test_rental_program.py
@@ -1,11 +1,12 @@
-""" Module docstring """
+""" Module Docstring """
 import rental_program
 
 def test_edit_inventory():
     """ Takes the user’s input and tells the inventory what items to take out/put in. """
-    assert rental_program.edit_inventory("sdsaj") == "Make sure you spell it correctly!"
-    assert rental_program.edit_inventory("actual item") == "Grabbing item..."
-    assert rental_program.edit_inventory('out of stock item') == "Sorry, that item is out of stock!"
+    assert rental_program.edit_inventory("sdsaj", "dkj") == "Error!"
+    assert rental_program.edit_inventory("actual item", "renting") == "Grabbing item..."
+    assert rental_program.edit_inventory("actual item", "returning") == "Restocking item..."
+    assert rental_program.edit_inventory('out of stock item', "renting") == "Sorry, that item is out of stock!"
 
 def test_transaction_history():
     """ Takes the user’s input and adds new info to the transaction history. """
@@ -20,6 +21,7 @@ def test_th_question():
     view inventory, make a transaction, or quit. """
     assert rental_program.th_question() == "i"
     assert rental_program.th_question() == "jkaf"
+    # should return t, i, h, or q
 
 def test_r_question():
     """ Asks the user whether they’re renting, returning, or replacing an item. """
@@ -27,9 +29,10 @@ def test_r_question():
 
 def test_item_question():
     """ Asks the user which item they are renting/returning/replacing. """
-    assert rental_program.item_question() is False
-    # type of this equals string
+    assert rental_program.item_question() == "one of the three r's"
+    # type of rental_program.item_question() equals string
 
 def test_return_price():
     """ Returns the price to the user. """
     assert rental_program.return_price(50) == "50"
+    #  should take in the price and return a string with the price in it
