9b3afd1605c3cfb7790c9f2cce609bdccf7e7569
James Sibert <jsibert@basecampcodingacademy.org>
2017-02-03 10:59:12 -0600

    added new notes to the functions, making sure I know what each is returning.

--- rental_program.py
+++ rental_program.py
@@ -1,29 +1,52 @@
-""" oh hi """
+""" Module Docstring """
 
-def edit_inventory(words):
-    """ hi """
-    return words
+def edit_inventory(words, status):
+    """
+    Takes the user’s input and tells the inventory what items to take out/put in.
+    """
+    if status == "i":
+        words = 4
+        new_info = words + 1
+    return new_info
+    # words should be the name of an item and status is either renting/returning/replacing
 
 def transaction_history():
-    """ hi """
-    return "afds"
+    """
+    Takes the item info and status and updates the transaction history.
+    """
+    return "what does this return?"
+    # change transaction history
 
 def show_inventory():
-    """ hi """
+    """
+    Returns inventory to the user.
+    """
+    # open inventory and save as a variable. Return that variable
     return False
 
 def th_question():
-    """ hi """
+    """
+    If t: make transaction
+    If h: show transaction history
+    If i: show inventory
+    If q: quit program
+    """
     return "hi"
 
 def r_question():
-    """ hi """
-    return "hi"
+    """
+    Asks the user whether they’re renting, returning, or replacing an item.
+    """
+    return "what the user says"
 
 def item_question():
-    """ hi """
-    return False
+    """
+    Asks the user which item they are renting/returning/replacing.
+    """
+    return "name of the item"
 
 def return_price(pri):
-    """ hi """
+    """
+    Returns the price to the user.
+    """
     return str(pri)
