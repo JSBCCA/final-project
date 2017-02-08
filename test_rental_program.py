ebe161a6e674047b20b09cb06e6d7d734b880bfe
James Sibert <jsibert@basecampcodingacademy.org>
2017-02-08 15:23:32 -0600

    No changes

--- test_rental_program.py
+++ test_rental_program.py
@@ -4,19 +4,19 @@ import rental_program
 
 def test_edit_inventory():
     """ Takes the user’s input and tells the inventory what items to take out/put in. """
-    assert rental_program.edit_inventory("sdsaj", "dkj") == "Error updating inventory."
-    assert rental_program.edit_inventory("actual item",
-                                         "renting") == "Grabbing item..."
-    assert rental_program.edit_inventory("actual item",
-                                         "returning") == "Restocking item..."
-    assert rental_program.edit_inventory("actual item",
-                                         "replacing") == "Replacing item..."
-    assert rental_program.edit_inventory('out of stock item',
-                                         "renting") == """Sorry, that item is out of stock!"""
+    assert rental_program.edit_inventory(["sdsaj", "dkj"]) == "Error!"
+    assert rental_program.edit_inventory(["actual item",
+                                          "renting"]) == "Grabbing item..."
+    assert rental_program.edit_inventory(["actual item",
+                                          "returning"]) == "Restocking item..."
+    assert rental_program.edit_inventory(["actual item",
+                                          "replacing"]) == "Replacing item..."
+    assert rental_program.edit_inventory(['out of stock item',
+                                          "renting"]) == "Sorry, that item is out of stock!"
 
 
 def test_edit_transaction_history():
     """ Takes the user’s input and adds new info to the transaction history. """
-    assert rental_program.edit_transaction_history("item", "renting") == "Added."
-    assert rental_program.edit_transaction_history("item", "returning") == "Added."
-    assert rental_program.edit_transaction_history("jdanj", "klasfj") == "Error adding transaction."
+    assert rental_program.edit_transaction_history(["item", "renting"]) == "Added."
+    assert rental_program.edit_transaction_history(["item", "returning"]) == "Added."
+    assert rental_program.edit_transaction_history(["jdanj", "klasfj"]) == "Error!"
